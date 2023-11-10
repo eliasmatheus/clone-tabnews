bbdc3a6d0386c1e631d416147a1dfac7f4a36a1b
Elias Matheus Oliveira <elias.matheus@rainforest.tech>
2023-11-10 17:08:46 -0300

    first challenge

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-  return <div>Amor, te amo ❤️</div>
+  return (
+    <div>
+      <h1>Amor,</h1>
+
+      <p>você faz os meus dias mais felizes 😍</p>
+      <p>com você o meu riso é certo 😁</p>
+      <p>você me faz sentir amado 😘</p>
+      <p>obrigado por escolher passar os resto dos seus dias ao meu lado 🥰</p>
+
+      <h2>Te amo, daqui até o céu ❤️</h2>
+    </div>
+  );
 }
 
 export default Home;
