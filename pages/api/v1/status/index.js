32a365ee88b8d83c2f2b7c1cf340cef6757a2b3e
Elias Matheus Oliveira <elias.matheus@rainforest.tech>
2024-03-03 17:57:59 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,34 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
-async function index(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log("=>(index.js:5) result", result.rows);
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).json({ chave: "são acima da média" });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections
+  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
-export default index;
+export default status;
