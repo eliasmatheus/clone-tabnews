9a6c3efff45014ee2beeacec9991a5493040018d
Elias Matheus Oliveira <elias.oliveira@ewally.com.br>
2024-10-03 11:56:44 -0300

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,20 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
-  await client.connect();
+  console.log("Credentiais do Postgress:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     return await client.query(queryObject);
   } catch (e) {
     console.error(e);
+    throw e;
   } finally {
     await client.end();
   }
